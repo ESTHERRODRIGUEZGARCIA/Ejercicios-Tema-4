ed3dab93289d672a656df9820946681689a184a5
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-11-19 21:32:24 +0100

    ;

--- ejer1.py
+++ ejer1.py
@@ -3,6 +3,14 @@ from time import time
 
 class Node:
     #declarar variables
+    prob = ''
 
 class Huffman:
     #distintas variables
+    tree = None
+    Root = None
+
+    def __init__(self, tree=None, root=None):
+        self.tree = tree
+        self.root = root
+        
