2ad9aae6f98cf5f5815b8851a14d078869c1e93f
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-11-20 11:49:55 +0100

    función/commit

--- huffman_archivo.py
+++ huffman_archivo.py
@@ -5,4 +5,9 @@ class Node(object):
         self.left = left
         self.right = right
 
-    
+    #Anulo función `__lt__()` para hacer que la clase `Nodo` funcione con la cola de prioridad de modo que el elemento de mayor prioridad tenga la frecuencia más baja
+
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+        
