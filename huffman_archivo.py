1b57f0ef7758aa5202f77a2306f6984352315762
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-11-20 12:06:26 +0100

    descodificando

--- huffman_archivo.py
+++ huffman_archivo.py
@@ -1,3 +1,4 @@
+
 class Node(object):
     def __init__(self, ch, freq, left=None, right=None):
         self.ch = ch
@@ -10,10 +11,12 @@ class Node(object):
     def __lt__(self, other):
         return self.freq < other.freq
 
-    #creo árbol y almaceno los códigos de Huffman en un diccionario
+
 
 def leaf(root):
     return root.left is None and root.right is None
+
+#creo árbol y almaceno los códigos de Huffman en un diccionario
 
 def encode(root, s, huffman_code):
     if root is None:
@@ -25,3 +28,24 @@ def encode(root, s, huffman_code):
     encode(root.left, s + '0', huffman_code)
     encode(root.left, s + '1', huffman_code)
 
+def decode(root, index, s):
+    if root is None:
+        return index
+    
+    if leaf(root):
+        print(root.ch, end='')
+        return index
+    
+    index = index + 1
+    root = root.left if s[index] == '0' else root.right
+    return decode(root, index, s)
+
+    # Construye 'Huffman Tree' y decodifica el texto de entrada dado
+
+def HuffmanTree(text):
+    if len(text) == 0: # cadena vacía
+        return None
+
+    freq = {i: text.count(i) for i in set(text)} #freq de cada caracter + dic
+
+    pq = [Node(k,v) for k, v in freq.items()]
