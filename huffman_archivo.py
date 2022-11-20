41c322d2a02d0e0d9850f90a7942c61a0060ee10
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-11-20 09:18:23 +0100

    2 archivos

---
+++ huffman_archivo.py
@@ -0,0 +1,16 @@
+import os
+from time import time
+
+class Node:
+    #declarar variables
+    prob = ''
+
+class Huffman:
+    #distintas variables
+    tree = None
+    Root = None
+
+    def __init__(self, tree=None, root=None):
+        self.tree = tree
+        self.root = root
+        
---
+++ huffman_entrada.py
@@ -0,0 +1,11 @@
+print("####################################")
+print("##      Algoritmo de Huffman      ##")
+print("####################################")
+
+my_string = input("\nIntroduzca una cadena de caracteres para realizar su compresion: \n")
+len_string = len(my_string)
+
+print("Su mensaje es: \n")
+print(my_string)
+
+ 
