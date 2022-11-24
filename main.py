964b095cc3dbb11d2f286c06c78c92f3a56f1510
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-11-24 08:27:52 +0100

    creo el main del 3

---
+++ main.py
@@ -0,0 +1,19 @@
+''' La Gran Muralla (China) Parte de la Gran Muralla China. ...
+Petra (Jordania) La fachada principal de Petra (Jordania)EM. ...
+El Coliseo (Italia) El Coliseo de Roma (Italia)EM. ...
+Chichen Itza (México) El Chichen Itza en México. ...
+Machu Picchu (Perú) ...
+El Cristo Redentor (Brasil) ...
+Taj Mahal (India)
+'''
+
+#from grafo import grafo
+
+grafo = Grafo("LAS 7 MARAVILLAS DEL MUNDO")
+grafo.agregar("La Gran Muralla")
+grafo.agregar("Petra")
+grafo.agregar("El Coliseo")
+grafo.agregar("Chichen Itza")
+grafo.agregar("EL Cristo Redentor")
+grafo.agregar("Taj Mahal")
+
