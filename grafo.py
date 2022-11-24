b6dca9dcf8a69a199b50ccdb48e85ebe2aa64b62
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-11-24 08:48:51 +0100

    grafo funciones

--- grafo.py
+++ grafo.py
@@ -1,3 +1,36 @@
+from nodo3 import Nodo
+
+class Grafo:
+    def __init__(self, info):
+        self.raiz = Nodo(info)
+
+    def arbol_vacio(raiz):
+        return raiz is None
+
+    def __agregar_recursivo(self, nodo, info):
+        if info < nodo.info:
+            if nodo.izquierda is None:
+                nodo.izquierda = Nodo(info)
+            else:
+                self.__agregar_recursivo(nodo.izquierda, info)
+        else:
+            if nodo.derecha is None:
+                nodo.derecha = Nodo(info)
+            else:
+                self.__agregar_recursivo(nodo.derecha, info)
+
+
+    def __buscar(self, nodo, busqueda):
+        if nodo is None:
+            return None
+        if nodo.info == busqueda:
+            return nodo
+        elif busqueda < nodo.info:
+               return self.__buscar(nodo.izquierda, busqueda)
+        else:
+            return self.__buscar(nodo.derecha, busqueda)
+
+
 
 
 
