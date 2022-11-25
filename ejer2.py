c6a8dd4b6a2474e7c201bbcfbc9dfb81f286200a
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-11-25 20:30:27 +0100

    fin 2, apartado 4 no va

--- ejer2.py
+++ ejer2.py
@@ -2,7 +2,6 @@ import csv
 with open('pokemon.csv', 'r') as f:
     reader = csv.reader(f)
     lista = list(reader)
-    
 
 
 nombres = []
@@ -28,7 +27,7 @@ print(tipo)
 
 
 
-# mostrar todos los datos de un Pokémon a partir de su número y nombre –para este último, la búsqueda debe ser por proximidad, es decir si busco “bul” se deben mostrar todos los Pokémons cuyos nombres comiencen o contengan dichos caracteres
+# 1. mostrar todos los datos de un Pokémon a partir de su número y nombre –para este último, la búsqueda debe ser por proximidad, es decir si busco “bul” se deben mostrar todos los Pokémons cuyos nombres comiencen o contengan dichos caracteres
 def buscar_pokemon():
     pokemon = input("Ingrese el nombre del pokemon que desea buscar: ")
     for i in range(len(lista)):
@@ -36,7 +35,7 @@ def buscar_pokemon():
             print(lista[i])
 
 
-# mostrar todos los nombres de todos los Pokémons de un determinado tipo agua, fuego, planta y eléctrico;
+# 2. mostrar todos los nombres de todos los Pokémons de un determinado tipo agua, fuego, planta y eléctrico;
 def buscar_tipo():
     tipo = input("Ingrese el tipo de pokemon que desea buscar: ")
     for i in range(len(lista)):
@@ -44,12 +43,12 @@ def buscar_tipo():
             print(lista[i][0])
 
 
-#realizar un listado en orden ascendente por número y nombre de Pokémon, y además un listado por nivel por nombre;
+#3. realizar un listado en orden ascendente por número y nombre de Pokémon, y además un listado por nivel por nombre;
 def ordenar_pokemon():
     lista.sort(key=lambda x: x[1])
     print(lista)
 
-#mostrar todos los Pokémons que son débiles frente a Jolteon, Lycanroc y Tyrantrum;
+#4. mostrar todos los Pokémons que son débiles frente a Jolteon, Lycanroc y Tyrantrum;
 def debiles():
     debiles = ["Jolteon", "Lycanroc", "Tyrantrum"]
     for i in range(len(lista)):
@@ -57,7 +56,9 @@ def debiles():
             if debiles[j] in lista[i][3]:
                 print(lista[i][0])
 
-# mostrar todos los tipos de Pokémons y cuántos hay de cada tipo.
+
+
+# 5. mostrar todos los tipos de Pokémons y cuántos hay de cada tipo.
 def tipos():
     tipos = []
     for i in range(len(lista)):
@@ -66,13 +67,12 @@ def tipos():
 tipos()
 
 def main2():
-    print("1. Buscar pokemon")
-    print("2. Buscar tipo")
-    print("3. Ordenar pokemon")
-
-    print("4. Debiles")
-    print("5. Tipos")
-    opcion = int(input("Ingrese una opcion: "))
+    print("\n1. Buscar pokemon. ")
+    print("2. Buscar tipo. ")
+    print("3. Ordenar pokemon. ")
+    print("4. Débiles. ")
+    print("5. Tipos. ")
+    opcion = int(input("Ingrese una opción: "))
     if opcion == 1:
         buscar_pokemon()
     elif opcion == 2:
