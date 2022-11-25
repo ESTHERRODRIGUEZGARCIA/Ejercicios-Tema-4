5f5ae3b6e86ca1108f638a4cf3d501f45e92c4a6
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-11-25 20:13:54 +0100

    .

--- ejer2.py
+++ ejer2.py
@@ -2,6 +2,11 @@ import csv
 with open('pokemon.csv', 'r') as f:
     reader = csv.reader(f)
     lista = list(reader)
+    print(lista)
+    print(lista[0])
+    print(lista[1])
+    print(lista[2])
+
 
 nombres = []
 for i in range(len(lista)):
@@ -23,3 +28,15 @@ for i in range(1, len(lista)):
 
 print("\n\n##### TIPOS #####\n\n")
 print(tipo)
+
+
+
+# mostrar todos los datos de un Pokémon a partir de su número y nombre –para este último, la búsqueda debe ser por proximidad, es decir si busco “bul” se deben mostrar todos los Pokémons cuyos nombres comiencen o contengan dichos caracteres
+def buscar_pokemon():
+    pokemon = input("Ingrese el nombre del pokemon que desea buscar: ")
+    for i in range(len(lista)):
+        if pokemon in lista[i][1]:
+            print(lista[i])
+buscar_pokemon()
+
+
